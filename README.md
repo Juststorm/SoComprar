8fad9be7f0a54bed57b593fcd71defc1b9ce9954
Lucas Lemos <lcslemosp14@gmail.com>
2022-09-21 23:35:10 -0300

    Versão final

--- README.md
+++ README.md
@@ -1,2 +1,51 @@
-# SoComprar
-Aplicação web progressiva desenvolvida para a disciplina de Tópicos Especiais em Sistemas de Informação: Aplicações Híbridas
+# Dream Hardware - Lucas Lemos Pinheiro
+
+Dream Hardware é minha primeira aplicação web progressiva. Esta aplicação serve para os entusiastas de hardware e PCs montarem uma "lista de desejo" com as peças que pretendem comprar, bem como seu preço. Ela disponibiliza a quantidade de peças catalogadas e também o total daquele orçamento do usuário.
+
+
+## Telas
+Esta aplicação é composta de três telas.
+### Tela inicial
+
+A primeira delas é a tela de pesquisa ou inicial, que pode ter dois comportamentos.
+
+Respectivamente, conforme imagens abaixo, o primeiro comportamento é de uma lista vazia, sem peças de hardware cadastrada. O segundo comportamento é com peças cadastradas, onde é disponibilizada uma barra de busca, especialmente útil caso o usuário possua uma lista de peças muito grande.
+
+![Tela4](screens/tela4.png)
+![Tela 1](screens/tela1.png)
+
+### Tela de cadastro
+
+A tela de cadastro, conforme abaixo, é onde o usuário pode incluir uma nova peça para sua lista de hardware/orçamento. Ele pode incluir a descrição da peça, tendo uma orientação quanto a uma nomenclatura, e também o seu valor.
+
+![Tela 3](screens/tela3.png)
+
+### Tela de edição
+
+Por fim, a aplicação possui uma tela de edição, onde o usuário pode editar os detalhes anteriormente cadastrados na tela de cadastro, e pode também excluir aquele registro.
+
+![Tela 2](screens/tela2.png)
+
+## Dados armazenados
+
+Esta aplicação tem o foco em armazenar peças de hardware, como placas de vídeo, SSDs, HDs, CPUs (processadores), coolers etc.. Para aqueles que sonham em montar um PC gamer, pode ser o ponto de partida manter e controlar um orçamento com essas peças de computador.
+
+## Respostas às perguntas
+
+- A aplicação é original e não uma cópia da aplicação de um colega ou de uma aplicação já existente?
+  >É uma aplicação original. Foi utilizada apenas a base ensinada pelo professor ao decorrer das aulas, mas a ideia é original e não foi consultada nenhuma outra aplicação para construi-la.
+- A aplicação tem pelo menos duas interfaces (telas ou páginas) independentes?
+  >Sim, a aplicação possui três telas/páginas.
+- A aplicação armazena e usa de forma relevante dados complexos (mais de um atributo)?
+  >Sim, a aplicação armazena uma string e valor numérico. Este valor numérico é convertido depois para float para permitir soma e casas decimais.
+- A aplicação possui um manifesto para instalação no dispositivo do usuário?
+  >Sim, a aplicação possui um manifesto disponível, conforme ensinado nas aulas.
+- A aplicação possui um service worker que permite o funcionamento off-line?
+  >Sim, a aplicação possui um service worker disponível, conforme ensinado nas aulas.
+  
+- O código da minha aplicação possui comentários explicando cada operação?
+  >Sim, o código está satisfatoriamente comentado.
+- A aplicação está funcionando corretamente?
+  >Sim, a aplicação funciona conforme esperado.
+- A aplicação está completa?
+  >Sim, a aplicação está completa e performa as ações que foram pensadas para ela, baseado principalmente no que foi ensinado nas aulas gravadas.
--- dreamhardware.js
+++ dreamhardware.js
@@ -1,7 +1,8 @@
+//esvazia vetor de peças
 let pecas = [];
-
+//operações realizadas no carregamento da aplicação
 onload = () => {
-    
+    //armazena localmente o vetor de peças
     const p = JSON.parse(localStorage.getItem('pecas'));
     if(p)
         pecas = p;
@@ -9,62 +10,120 @@ onload = () => {
     mostraPecas();
     document.querySelector('#inputNovaPeca').oninput = monitoraCampoAdic;
 
+    //botão que leva ao cadastero de peças
     document.querySelector('#buttonAdic').onclick = () => {
         document.querySelector('#buttonIncluir1').disabled = true;
         ativa('tela2');
         document.querySelector('#inputNovaPeca').focus();
     };
 
+    //botão que cancela operação atual e desfaz operações, voltando ao estado inicial
     document.querySelector('#buttonCanc1').onclick = () => {
         document.querySelector('#inputNovaPeca').value = '';
         ativa('tela1');
     }
 
+    //botão que cancela operação atual e desfaz operações, voltando ao estado inicial
     document.querySelector('#buttonCanc2').onclick = () => {
         let campo = document.querySelector('#inputEditarPeca');
+        let campo2 = document.querySelector('#inputEditarValorPeca');
         campo.value = '';
+        campo2.value = '';
         campo.removeAttribute('data-id', '');
         ativa('tela1');
     }
 
+    //acionamento do botão de inclusão de peça no vetor
     document.querySelector('#buttonIncluir1').onclick = () => {
         adicionaPeca();
     };
 
+    //acionamento do botão chama a função que edita a peça do vetor
     document.querySelector('#buttonSalvarAlt').onclick = () => {
         alteraPeca();
     };
+    //acionamento do botão chama a função que apaga a peça do vetor
     document.querySelector('#buttonExcluir').onclick = () => {
         apagaPeca();
     };
+
 }
 
+//barra de busca para elementos da lista. converte tudo digitado para minusculo, para não ter problemas na correspondencia.
+//aplica estilo de não exibição para itens não correspondentes
+function barraBusca() {
+    let input = document.getElementById('barraDeBusca').value
+    input=input.toLowerCase();
+    let p = document.getElementsByClassName('peca');
+      
+    for (i = 0; i < p.length; i++) { 
+        if (!p[i].innerHTML.toLowerCase().includes(input)) {
+            p[i].style.display="none";
+        }
+        else {
+            p[i].style.display="list-item";                 
+        }
+    }
+}
+
+//função para mostrar as peças, chamada diversas vezes. Percorre o vetor e exibe as peças na tela, com as devidas informações. Usa elementos DOM
 const mostraPecas = () => {
     const listaDePecas = document.querySelector('#listaDePecas');
     listaDePecas.innerHTML = '';
     pecas.forEach((p) => {
         let elemPeca = document.createElement('li');
+        elemPeca.classList.add('peca');
         elemPeca.innerHTML = p.descricao + ' - R$ ' + p.preço;
         elemPeca.setAttribute('data-id', p.id);
         elemPeca.onclick = () => {
             let campo = document.querySelector('#inputEditarPeca');
+            let campo2 = document.querySelector('#inputEditarValorPeca');
             ativa('tela3');
             campo.value = p.descricao;
+            campo2.value = p.preço;
             campo.setAttribute('data-id', p.id);
             campo.focus();
         }
         listaDePecas.appendChild(elemPeca);
+
     });
-    document.querySelector('#estado').innerText = pecas.length;
+    //document.querySelector('#estado').innerText = pecas.length;
+    //Alterna exibição de componentes da tela de acordo com a quantidade de peças. Se vazio, não exibe nada.
     if(pecas.length > 0) {
         listaDePecas.classList.remove('hidden');
         document.querySelector('#blank').classList.add('hidden');
+        document.querySelector('#qtdPecas').classList.remove('hidden');
+        document.querySelector('#valorTotalPecas').classList.remove('hidden');
+        document.querySelector('#barraDeBusca').classList.remove('hidden');
+
     } else {
         listaDePecas.classList.add('hidden');
         document.querySelector('#blank').classList.remove('hidden');
+        document.querySelector('#qtdPecas').classList.add('hidden');
+        document.querySelector('#valorTotalPecas').classList.add('hidden');
+        document.querySelector('#barraDeBusca').classList.add('hidden');
     }
+
+    //Função que conta a quantidade de elementos do vetor e também realiza a soma dos valores das peças, disponibilizando o total do orçamento.
+    const contaPecas = () => {
+        let total = pecas.length;
+        let valorTotal = 0;
+        pecas.forEach((p) => {
+            valorTotal += parseFloat(p.preço);
+        });
+        document.querySelector('#qtdPecas').innerText = 'Quantidade de peças: ' + total;
+        document.querySelector('#valorTotalPecas').innerText = 'Valor total do orçamento: R$ ' + valorTotal.toFixed(2);
+    }
+
+    contaPecas(pecas);
+
+    //função descontinuada de sort
+    //console.log(pecas.sort(function (a, b) {
+    //   return a.preço - b.preço}));
+
 }
 
+//função principal para ativação de componentes na tela
 const ativa = (comp) => {
     let listaDeTelas = document.querySelectorAll('body > .component');
     listaDeTelas.forEach((c) => 
@@ -72,6 +131,7 @@ const ativa = (comp) => {
         document.querySelector('#' + comp).classList.remove('hidden');    
 }
 
+//função para cadastrar no vetor a peça inputada pelo usuário
 const adicionaPeca = () => {
     let campo = document.querySelector('#inputNovaPeca');
     let campo2 = document.querySelector('#inputValorPeca');
@@ -91,24 +151,30 @@ const adicionaPeca = () => {
     }
 }
 
+//Verifica e habilita o botão para utilização e cadastro de peça
 const monitoraCampoAdic = (e) => {
     let botao = document.querySelector('#buttonIncluir1');
     if(e.target.value.length > 0) botao.disabled = false;
     else botao.disabled = true;
 }
 
+//Função para alteração da peça cadastrada. Traz os valores originais e permite salvar ao final a alteração.
 const alteraPeca = () => {
     let campo = document.querySelector('#inputEditarPeca');
+    let campo2 = document.querySelector('#inputEditarValorPeca');
     let idPeca = campo.getAttribute('data-id');
     let i = pecas.findIndex((p) => p.id == idPeca);
     pecas[i].descricao = campo.value;
+    pecas[i].preço = campo2.value;
     campo.value = '';
+    campo2.value = '';
     campo.removeAttribute('data-id', '');
     ativa('tela1');
     salvaPecas();
     mostraPecas();
 }
 
+//Função para remover a peça selecionada do vetor
 const apagaPeca = () => {
     let campo = document.querySelector('#inputEditarPeca');
     let idPeca = campo.getAttribute('data-id');
@@ -120,8 +186,10 @@ const apagaPeca = () => {
     mostraPecas();
 }
 
+//Função que salva em armazenamento local as peças no vetor
 const salvaPecas = () => {
     localStorage.setItem('pecas', JSON.stringify(pecas));
 }
 
+//registro do service worker para funcionamento off-line
 navigator.serviceWorker.register('./sw.js');
